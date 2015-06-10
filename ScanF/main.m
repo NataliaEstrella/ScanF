85407e4722699d2751adc149fb8d9853b5a00f36
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-09 20:48:35 -0400

    Initial Commit

---
+++ ScanF/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ScanF
+//
+//  Created by Natalia Estrella on 6/9/15.
+//  Copyright (c) 2015 Natalia Estrella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
