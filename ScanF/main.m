e305dbf63498af8d85eac89bfd9d9b93681a6214
Natalia Estrella <natalia.c.estrella@gmail.com>
2015-06-10 22:36:45 -0400

    Scan F Notes

--- ScanF/main.m
+++ ScanF/main.m
@@ -10,8 +10,39 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+       
+//        int age;
+//        int faveNumber;
+//        scanf("%d%d", &age, &faveNumber);
+//        NSLog(@"You are %d years old %d!", age, faveNumber);
+//    }
+        
+//        int i;
+//        int r;
+//        int t;
+//        scanf("%d%d%d", &i, &r, &t);
+//        NSLog(@"Value: = %i", i + r  + t);
+//}
+    
+//    char name[  256];
+//    int age;
+//    
+//    scanf("%d", &age);
+//    NSLog(@"You are %d years old!", age);
+//    
+//    scanf("%s", &name);
+//    NSLog(@"And your name is %s!", name);
+    
+    char noun[256];
+    char verb[256];
+    char holiday[256];
+    int i;
+    
+        scanf("%s%s%s%d", &noun, &verb, &holiday, &i);
+        NSLog(@"%s is awesome. I %s them all the time! Next %s you're getting a basket of %d", noun, verb, holiday, i);
+    
     }
+
     return 0;
-}
+
+ }
